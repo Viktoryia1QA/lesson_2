063ca3729a724b4c4009f558f6ee996b61e260c6
Viktoryia1QA <ViktoryiaAntonchyk@gmail.com>
2022-02-03 02:28:45 +0300

    add file first.js

---
+++ first.js
@@ -0,0 +1,17 @@
+const fse = require('fs-extra');
+
+fse.ensureDirSync('./firstFolder');
+fse.ensureDirSync('./secondFolder');
+
+fse.ensureFileSync('./firstFolder/file.txt');
+
+fse.moveSync('./firstFolder/file.txt', './secondFolder/file.txt');
+
+fse.removeSync('./secondFolder/file.txt');
+
+fse.removeSync('./firstFolder');
+fse.removeSync('./secondFolder');
+
+console.log('lesson 1 is finished');
+
+console.log('www');
